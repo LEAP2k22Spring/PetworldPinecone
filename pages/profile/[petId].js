04f92b8477895c66579330ba83722dd820c028eb
chinkhuslen <chinkhuslen10@gmail.com>
2022-11-18 11:16:44 +0800

    profile edited

--- pages/profile/[petId].js
+++ pages/profile/[petId].js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useFirebase } from "../../firebase/useFirebase";
 import LoadingSpinner from "../../component/Spinner";
 import { useRouter } from "next/router";
-// import { PetProfile } from "../../component";
+import { PetProfile } from "../../component/pet";
 
 const Pet = () => {
   const router = useRouter();
@@ -25,7 +25,7 @@ const Pet = () => {
   return (
     <div>
       <LoadingSpinner open={isLoading} />
-      {/* <PetProfile petData={petData} /> */}
+      <PetProfile petData={petData} />
     </div>
   );
 };
