d3f0ef59f402f04185c2ed0e3572c837c49b9fc5
Munkhbold08 <74584745+Munkhbold08@users.noreply.github.com>
2022-11-21 09:41:00 +0800

    comment done

--- component/explorePost.js
+++ component/explorePost.js
@@ -10,12 +10,15 @@ import { red } from '@mui/material/colors';
 import moment from "moment"
 import React, { useEffect, useState } from "react";
 import { auth, db, useCollection } from "../firebase/useFirebase";
-import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, orderBy, query, serverTimestamp, setDoc } from "firebase/firestore";
 import { useGetUsersDataContext } from "../context/UsersDataContext";
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import AddReactionIcon from '@mui/icons-material/AddReaction';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { textAlign } from "@mui/system";
+import { async } from "@firebase/util";
 
 
 
@@ -24,6 +27,7 @@ const Post = ({ id, userAvatar, createdAt, desc, userName, image, userID }) => {
     const [hasLikes, setHasLikes] = useState(false);
     const { getUsersData } = useGetUsersDataContext();
     const [comment, setComment] = useState("");
+    const [comments, setComments] = useState([]);
     const [user] = useAuthState(auth);
     const router = useRouter();
 
@@ -39,6 +43,17 @@ const Post = ({ id, userAvatar, createdAt, desc, userName, image, userID }) => {
         () => setHasLikes(likes.findIndex((like) => like.id === user.uid) !== -1),
         [likes]
     );
+
+
+    useEffect(
+        () =>
+        onSnapshot(collection(db, "Posts", id, "comments"), (snapshot) => {
+            setComments(snapshot.docs)
+            console.log("hey", snapshot.docs);
+        }
+        ),[db, id]
+      );
+
     const likePost = async () => {
         try {
             if (hasLikes) {
@@ -54,6 +69,39 @@ const Post = ({ id, userAvatar, createdAt, desc, userName, image, userID }) => {
             console.log(error);
         }
     };
+
+    const sendComment = async (e) => {
+        e.preventDefault();
+        /*     setLoading(true); */
+    
+        try {
+          await addDoc(collection(db, "Posts", id, "comments"), {
+            comment: comment,
+            userName: getUsersData.firstName,
+            userAvatar: getUsersData.avatar,
+            createdAt: serverTimestamp(),
+            userId: user.uid
+          });
+          console.log("Done");
+        } catch (error) {
+          console.log(error);
+        }
+    
+        setComment("");
+        /*  setLoading(false); */
+      };
+    
+    const deleteComment = async (e)=>{
+        try {
+            await deleteDoc(doc(db, "Posts", id, "comments", e));
+        } catch (error) {
+            
+        }
+    }
+
+
+
+
     const openAddPetHandler = (docId) => {
         router.push(`/profile/posts/${docId}`);
     };
@@ -101,11 +149,28 @@ const Post = ({ id, userAvatar, createdAt, desc, userName, image, userID }) => {
                         <BookmarkIcon sx={{ color: 'white' }} />
                     </IconButton>
                 </CardActions>
+                {comments.map((comment, i)=>(
+                    <CardActions key={i} sx={{ justifyContent: 'space-between', alignItems:"center", justifyItems:"center"}}>
+                            <Box display="flex" gap={1} justifyContent="center" alignItems="center">
+                                <Avatar sx={{ bgcolor: red[500], width: '30px', height: '30px' }} aria-label="recipe" src={comment.data().userAvatar}>
+                                </Avatar>
+                                <Typography fontSize={15} fontWeight={600}>{comment.data().userName}</Typography>
+                                <Typography fontSize={15}>{comment.data().comment}</Typography>
+
+                            </Box>
+                            <Typography fontSize={13}>{moment(new Date(comment.data().createdAt?.seconds * 1000)).fromNow()}</Typography>
+                            {user.uid === comment.data().userId ? <DeleteOutlineOutlinedIcon fontSize="small" onClick={()=>deleteComment(comment.id)} sx={{
+                            '&:hover': {
+                                color: 'rgb(96 165 250)', cursor:"pointer"
+                            }}}/>  : ""}
+                    </CardActions>
+                ))}
+                
                 <CardActions sx={{ justifyContent: 'space-between' }} >
                     <Typography></Typography>
                     <AddReactionIcon />
-                    <TextField size="small" placeholder="Add a comment..."></TextField>
-                    <IconButton>
+                    <TextField size="small" placeholder="Add a comment..." value={comment} onChange={(e) => setComment(e.target.value)}></TextField>
+                    <IconButton onClick={sendComment}>
                         <PostAddIcon sx={{
                             '&:hover': {
                                 color: 'rgb(96 165 250)'
