0e0b10eb4d4585b69b27c473bf08b8625dee0a29
Mandakh0114 <mandahmaagii525@gmail.com>
2022-11-23 20:34:20 +0800

    update

---
+++ pages/Uploadprofile.js
@@ -0,0 +1,42 @@
+import Avatar from "@mui/material/Avatar";
+import { useState } from "react";
+import { ref, uploadBytes, getDownloadURL, storage } from "firebase/storage";
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  const handleImageChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
+
+  const handleSubmit = () => {
+    const imageRef = ref(storage, "image");
+    uploadBytes(imageRef, image)
+      .then(() => {
+        getDownloadURL(imageRef)
+          .then((url) => {
+            setUrl(url);
+          })
+          .catch((error) => {
+            console.log(error.message, "error getting the image url");
+          });
+        setImage(null);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Avatar src={url} sx={{ width: 150, height: 150 }} />
+      <input type="file" onChange={handleImageChange} />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
