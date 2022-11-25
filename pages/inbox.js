6733b918b355dd09efbf9ab58e873dfcfcc13470
chinkhuslen <chinkhuslen10@gmail.com>
2022-11-25 11:35:45 +0800

    inbox dr coming soon gj bichsen ba css hiisen shuu

--- pages/inbox.js
+++ pages/inbox.js
@@ -1,13 +1,23 @@
-import { Box } from "@mui/material"
-
+import { Box } from "@mui/material";
 
 export default function Inbox() {
-
-    return (
-        <>
-            <Box>
-                
-            </Box>
-        </>
-    )
+  return (
+    <>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontWeight: "bold",
+          fontSize: "2rem",
+          bgcolor: "#000",
+          color: "#fff",
+        }}
+      >
+        Chat Coming Soon...
+      </Box>
+    </>
+  );
 }
