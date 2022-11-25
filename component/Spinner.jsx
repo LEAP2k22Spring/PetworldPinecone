dce0db573f1d298e733ddf662795573fe2a4b37f
chinkhuslen <chinkhuslen10@gmail.com>
2022-11-25 10:36:14 +0800

    merged everything

--- component/Spinner.jsx
+++ component/Spinner.jsx
@@ -1,21 +1,19 @@
 import { Backdrop, Stack } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 
-const LoadingSpinner = ({ open }) => {
+const LoadingSpinner = ({ open, bgcolor, color = '#fff' }) => {
   return (
-    <div>
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={open}
-      >
-        <Stack>
-          {/* <div style={{ marginBottom: '20px', fontSize: '18px' }}>
+    <Backdrop
+      sx={{ backgroundColor: bgcolor || 'inherit', color: color, zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      open={open}
+    >
+      <Stack>
+        {/* <div style={{ marginBottom: '20px', fontSize: '18px' }}>
             Loading...
           </div> */}
-          <CircularProgress color='inherit' />
-        </Stack>
-      </Backdrop>
-    </div>
+        <CircularProgress color='inherit' />
+      </Stack>
+    </Backdrop>
   );
 };
 
--- providers/AuthProvider.js
+++ providers/AuthProvider.js
@@ -1,16 +1,20 @@
-import { onAuthStateChanged, signOut } from 'firebase/auth';
-import { createContext, useContext, useEffect, useState } from 'react';
-import { auth, useDocument } from '../firebase/useFirebase';
-import Login from '../component/signin';
-import LandingPage from '../component/landingPage';
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth, useDocument } from "../firebase/useFirebase";
+import Login from "../component/signin";
+import LandingPage from "../component/landingPage";
+import LoadingSpinner from "../component/Spinner";
 
 const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   // const  = auth.currentUser;
   const [user, setUser] = useState(false);
-  const [checking, setChecking] = useState(true)
+  const [checking, setChecking] = useState(true);
   const [startBtnClick, setStartBtnClick] = useState(false);
-  const { data: userData, loading } = useDocument({ path: 'Users', docId: auth?.currentUser?.uid });
+  const { data: userData, loading } = useDocument({
+    path: "Users",
+    docId: auth?.currentUser?.uid,
+  });
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -28,30 +32,30 @@ export const AuthProvider = ({ children }) => {
     setStartBtnClick(true);
     signOut(auth)
       .then(() => {
-        console.log('Log-out success');
+        console.log("Log-out success");
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-
   return (
     <AuthContext.Provider value={{ user, logout, userData, loading }}>
       {checking && (
         <h1
           style={{
-            position: 'absolute',
-            zIndex: '1300',
-            width: '100%',
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            background: 'rgba(0,0,0,0.3)',
+            position: "absolute",
+            zIndex: "1300",
+            width: "100%",
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "#fff",
+            color: "#000",
           }}
         >
-          {/* LOADING... */}
+          <LoadingSpinner open={true} color="#000" />
         </h1>
       )}
 
