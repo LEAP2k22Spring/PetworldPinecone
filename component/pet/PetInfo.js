9e8781914d8183d141a5bd940c26aa330b0d394f
Temuujin Erdenebileg <102133245+temuujin2@users.noreply.github.com>
2022-11-26 14:22:40 +0800

    update pet profile

--- component/pet/PetInfo.js
+++ component/pet/PetInfo.js
@@ -1,5 +1,7 @@
 import { Paper, Typography, Stack } from '@mui/material';
+import Image from 'next/image';
 import classes from '../../component/profile.module.css';
+
 
 const PetInfo = ({ petData }) => {
   return (
@@ -11,6 +13,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fweight.png?alt=media&token=a27f0458-3be1-4385-a9c2-c5e25f699bec"
+            />
             <Typography sx={{ color: '#696969' }}>Weight (kg):</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.weight}
@@ -23,6 +31,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet height"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fheight1.png?alt=media&token=ec36eba3-1322-4bbb-a5b0-e8a611a37268"
+            />
             <Typography sx={{ color: '#696969' }}>Height (cm):</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.height}
@@ -35,6 +49,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fpets-allowed.png?alt=media&token=28cae717-d0dd-4fec-ae58-ce578e96455a"
+            />
             <Typography sx={{ color: '#696969' }}>Color:</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.color}
@@ -47,6 +67,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fcalendar_date.png?alt=media&token=6cd94fe6-74d0-48ce-a617-64f22279a077"
+            />
             <Typography sx={{ color: '#696969' }}>Birth date:</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.birthDate}
@@ -59,6 +85,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fcertificate.png?alt=media&token=faf3f13a-5190-46ff-bcb9-e6f9cf7927a8"
+            />
             <Typography sx={{ color: '#696969' }}>Breed:</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.breed}
@@ -71,6 +103,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fvaccination.png?alt=media&token=8a7ea45f-7ed1-4519-986a-7574bdbd6922"
+            />
             <Typography sx={{ color: '#696969' }}>Vaccinated:</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.vaccinated ? 'yes' : 'no'}
@@ -83,6 +121,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fmicrochip.png?alt=media&token=acbc6218-ae2f-4834-8a66-0f96a522ad18"
+            />
             <Typography sx={{ color: '#696969' }}>Microchipped:</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.microchipped ? 'yes' : 'no'}
@@ -95,6 +139,12 @@ const PetInfo = ({ petData }) => {
             justifyContent='flex-start'
             sx={{ padding: '10px 20px' }}
           >
+            <Image 
+              width={64}
+              height={64}
+              alt="pet weight"
+              src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fveterinarian.png?alt=media&token=11fa1d2e-ba13-47be-a912-d00d5999226c"
+            />
             <Typography sx={{ color: '#696969' }}>Sprayed:</Typography>
             <Typography sx={{ fontSize: '1rem', fontWeight: 700 }}>
               {petData?.sprayed ? 'yes' : 'no'}
