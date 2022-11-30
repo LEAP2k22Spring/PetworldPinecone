865899efdaafa4899346bfdf5dc31a11419b3c57
Temuujin Erdenebileg <102133245+temuujin2@users.noreply.github.com>
2022-11-30 10:10:52 +0800

    petInfo img size change

--- component/pet/PetInfo.js
+++ component/pet/PetInfo.js
@@ -15,8 +15,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fweight.png?alt=media&token=a27f0458-3be1-4385-a9c2-c5e25f699bec"
             />
@@ -36,8 +36,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet height"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fheight1.png?alt=media&token=ec36eba3-1322-4bbb-a5b0-e8a611a37268"
             />
@@ -57,8 +57,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fpets-allowed.png?alt=media&token=28cae717-d0dd-4fec-ae58-ce578e96455a"
             />
@@ -78,8 +78,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fcalendar_date.png?alt=media&token=6cd94fe6-74d0-48ce-a617-64f22279a077"
             />
@@ -99,14 +99,14 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fcertificate.png?alt=media&token=faf3f13a-5190-46ff-bcb9-e6f9cf7927a8"
             />
             <Box className={classes.petInfo_text}>
-            <Typography sx={{ color: '#696969', fontSize:'10px', marginTop:'4px' }}>Breed:</Typography>
-            <Typography sx={{ fontSize: '1rem', fontWeight: 700 , fontSize:'14px'}}>
+            <Typography sx={{ color: '#696969', fontSize:'10px', marginTop:'-5px' }}>Breed:</Typography>
+            <Typography sx={{ fontSize: '1rem', fontWeight: 700 , fontSize:'14px', lineHeight:'14px'}}>
               {petData?.breed}
             </Typography>
             </Box>
@@ -120,8 +120,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+             width={38}
+             height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fvaccination.png?alt=media&token=8a7ea45f-7ed1-4519-986a-7574bdbd6922"
             />
@@ -141,8 +141,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fmicrochip.png?alt=media&token=acbc6218-ae2f-4834-8a66-0f96a522ad18"
             />
@@ -162,8 +162,8 @@ const PetInfo = ({ petData }) => {
             sx={{ padding: '10px 0'}}
           >
             <Image 
-              width={40}
-              height={40}
+              width={38}
+              height={38}
               alt="pet weight"
               src="https://firebasestorage.googleapis.com/v0/b/petworldpinecone.appspot.com/o/icons%2Fveterinarian.png?alt=media&token=11fa1d2e-ba13-47be-a912-d00d5999226c"
             />
